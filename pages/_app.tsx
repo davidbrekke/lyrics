b678a17a441b1a4912f4227fc2bc1eb2dd07cce3
David Magnuson <52757864+davidbrekke@users.noreply.github.com>
2022-02-16 11:41:28 -0600

    configure react query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,12 @@
 import 'tailwindcss/tailwind.css'
+import { QueryClient, QueryClientProvider } from 'react-query'
+
+const queryClient = new QueryClient()
 
 export default function _App({ Component, ...pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  )
 }
